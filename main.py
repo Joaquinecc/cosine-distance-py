edd50af587978c5edd98813ad898ac483b071300
joaquinecc <joaquinecc@gmail.com>
2022-01-21 12:57:15 -0300

    parallel client loop

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from scipy.sparse import coo_matrix, vstack,csr_matrix
 from scipy.sparse import lil_matrix
 import numpy as np
 import time
+from multiprocessing import  Pool,cpu_count
 start_time = time.time()
 BYTES_TO_MB_DIV = 0.000001
 
@@ -90,7 +91,12 @@ def ranking_similarity_client(cx,index,N=10):
         temp.append({"Neighbor":index[j],"cosine distance":1-v})
     return data
 
-def rank_sku(df_ratings,rows,n_client):
+
+def rank_sku(args):
+    df_ratings,rows,n_client,N = args
+    print(n_client)
+    print(" %s seconds ---" % (time.time() - start_time))
+
     #Neighbor ratings products 
     df_rating_neighbor= df_ratings.loc[rows["nearest neighbor - <RowID>"]].drop(["Cluster_Cuantitativo"], axis=1)
     #drop producto columns fill with 0 (product that where not consumed)
@@ -128,6 +134,7 @@ def rank_sku(df_ratings,rows,n_client):
     df_temp["rank"]=np.arange(1,df_temp.shape[0]+1)
     df_temp=df_temp[df_temp["rank"]<=N]    
     return df_temp
+
 def main():
     print("Start..")
     print("--- %s seconds ---" % (time.time() - start_time))
@@ -177,10 +184,10 @@ def main():
     df_rank=pd.DataFrame(data,columns=["n_client","nearest neighbor - <RowID>","distance","nearest neighbor - index"])
     clients_group = df_rank.groupby("n_client")
     df_ranking_sku=pd.DataFrame([],columns=["weigth","producto","n_client","rank"])
-    for n_client, rows in clients_group:
-        print(n_client)
-        df_ranking_sku=df_ranking_sku.append(rank_sku(df_ratings,rows,n_client))
-        print("--- Time client %s seconds ---" % (time.time() - start_time))
+
+    with Pool(cpu_count()) as pool:
+        df_ranking_sku.append(pool.map( rank_sku, [(df_ratings,rows,n_client,N) for n_client, rows in clients_group]))
+
 
     #save
     df_ranking_sku.to_csv(path_write)
